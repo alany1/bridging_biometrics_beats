b72d888fa2c93df72fc65b233e4d5c858e2dc30e
Alan Yu <alanyu@csail.mit.edu>
2023-11-26 22:04:17 -0500

    Clean up text generator

--- watch_parser/textgenerator.py
+++ watch_parser/textgenerator.py
@@ -16,11 +16,12 @@ from params_proto.partial import proto_partial
 import pandas as pd
 from dataclasses import dataclass
 
+
 @dataclass
 class TextGenerator:
     dataset: str
 
-    def __call__(self):
+    def __call__(self, prompt):
         agent = create_csv_agent(
             ChatOpenAI(temperature=0, model="gpt-4-1106-preview"),
             self.dataset,
@@ -29,16 +30,17 @@ class TextGenerator:
         df = pd.read_csv(self.dataset)
         print(df)
 
-        tool_input = {"input": 
-            {"name": "python", 
-            "arguments": "This was a swimming workout. What genre of music would be good to listen to? Use all the data to come to a conclusion."
-            }
-        }
+        tool_input = {"input":
+                          {"name": "python",
+                           "arguments": prompt,
+                           }
+                      }
 
         out = agent.run(tool_input)
         return out
 
-if __name__ == '__main__':
-    gen = TextGenerator("example_data/swim.csv")
-    gen()
 
+if __name__ == '__main__':
+    prompt="This was a swimming workout. What genre of music would be good to listen to? Use all the data to come to a conclusion. Summarize it as a playlist description."
+    gen = TextGenerator("../example_data/swim.csv")
+    gen(prompt)
