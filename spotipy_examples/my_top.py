2cf6f41093e861a7a45869fcd6a100efe98dffbf
Alan Yu <alanyu@csail.mit.edu>
2023-11-26 22:05:59 -0500

    add killport

--- spotipy_examples/my_top.py
+++ spotipy_examples/my_top.py
@@ -1,7 +1,9 @@
 # Shows the top artists for a user
+from killport import kill_ports
+
+kill_ports(ports=[9090])
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
 scope = 'user-top-read'
 ranges = ['short_term', 'medium_term', 'long_term']
 
@@ -11,7 +13,7 @@ for sp_range in ['short_term', 'medium_term', 'long_term']:
     print("range:", sp_range)
 
     results = sp.current_user_top_artists(time_range=sp_range, limit=50)
-
+    
     for i, item in enumerate(results['items']):
         print(i, item['name'])
     print()
