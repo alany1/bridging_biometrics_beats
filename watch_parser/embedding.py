129e5e313b4bf7747b3419443204044a6b2b9f8c
Michael Peng <michaelpeng@Michaels-MacBook-Pro-2.local>
2023-12-10 02:53:31 -0500

    transformer using prebuilt models

--- watch_parser/embedding.py
+++ watch_parser/embedding.py
@@ -1,39 +1,19 @@
-from watch_parser.textgenerator import TextGenerator
-from langchain.agents.agent_types import AgentType
-from langchain.chat_models import ChatOpenAI
-from langchain.llms import OpenAI
-from langchain_experimental.agents.agent_toolkits import create_csv_agent
-import os
-from getpass import getpass
-from params_proto import PrefixProto
-from params_proto.partial import proto_partial
-import pandas as pd
 import pickle
 
 
-dataset = "example_data/swim_hr.csv"
-
-
-def embed_text(args):
+def embed_text(text):
     """
     Uses HuggingFace sentence-transformers/all-MiniLM-L6-v2 model to map sentences & paragraphs 
     to a 384 dimensional dense vector space for use as input to playlist generation model. 
     Returns vector space.
     """
     # Open saved pickle file 
-    with open('MiniLMTransformer.pkl', 'rb') as f:
+    with open('../MINILM12.pkl', 'rb') as f:
         embedder = pickle.load(f)
 
-    # Embed input text
-    #input_text = args.text
-    input_to_model = embedder.encode(args)
+    input_to_model = embedder.encode(text)
     return input_to_model
 
-# textgenerator = TextGenerator(dataset)
-output = "Considering the heart rate data from the swimming workout, which ranges from 95 to 99 beats per minute, a genre of music with a tempo in that range or slightly higher could be suitable. Genres like dance, electronic, or upbeat pop music often have songs within the 100-130 BPM range, which could be a good match for maintaining the energy level during the workout."
-print(type(output))
-print(output)
-print(embed_text(output))
 
 
 
--- watch_parser/embedding_to_feature.py
+++ watch_parser/embedding_to_feature.py
@@ -1,35 +1,36 @@
+import os
+import glob
+import pickle
 
-def generate_params(model_input, args):
+
+# we need to pass in our our dataset, textgenerator to get text, embedding to get embedding, and then run each model on the embedding
+def generate_params(embedding, popularity=None, verbose=False):
     """
     Takes vector space of dimension 384 and outputs a prediction for each of 12 audio parameters
     using 12 separate pre-trained XGBoostRegressor models.
     Returns a dictionary of predicted parameters, including desired genre and playlist length.
     """
-    # Parameters and genre
-
     parameters = ['acousticness', 'danceability', 'energy', 'instrumentalness',
                   'key', 'liveness', 'target_loudness', 'mode', 'speechiness',
                   'tempo', 'signature', 'valence']
 
     # Create dictionary to be added to (with popularity if argument has been passed)
-    if args.popularity:
-        input_to_spotify_transformer = {'target_popularity': args.popularity}
+    if popularity:
+        spotify_features = {'target_popularity': popularity}
     else:
-        input_to_spotify_transformer = {}
+        spotify_features = {}
 
     # Find the XGB files
-    xgboost_files = os.path.join("model_xgb_384/*_model_xgb_384")
+    xgboost_files = os.path.join("../model_xgb_384/*_model_xgb_384")
     xgboost_models = glob.glob(xgboost_files)
 
     # Use each XGB model to predict on corresponding audio parameter
     for parameter, model in zip(parameters, xgboost_models):
         with open(model, 'rb') as f:
             xgb_model = pickle.load(f)
-        preds = xgb_model.predict(model_input.reshape(1, -1))
-        input_to_spotify_transformer[parameter] = preds[0]
+        preds = xgb_model.predict(embedding.reshape(1, -1))
+        spotify_features[parameter] = preds[0]
 
-    if args.verbose > 0:
-        print(input_to_spotify_transformer)
-    return input_to_spotify_transformer
-
-    
+    if verbose:
+        print(spotify_features)
+    return spotify_features
